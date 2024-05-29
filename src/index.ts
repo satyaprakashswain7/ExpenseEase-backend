0fd6b96a4362f066b18303f702c9ad2506dea854
Programmer <Programmer@example.com>
2024-05-29 09:58:30 +0530

    pushing

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+//pwd: NOsVrIfgSVSjQMhF
+import express,{ Express } from "express"
+import mongoose from "mongoose"
+import financialRecordRouter from "./routes/financial-records"
+import cors from "cors"
+
+require('dotenv').config()
+const app: Express = express()
+const port = process.env.VITE_PORT  || 3000
+
+app.use(express.json())
+app.use(cors())
+
+const mongoURI: string = process.env.VITE_MONGO_URI || ''
+mongoose.connect(mongoURI)
+.then(() => console.log("connected to MongoDB succesfully"))
+.catch((err) => console.error("failed ",err))
+
+app.use("/financial-records",financialRecordRouter)
+
+app.listen(port,() => {
+    console.log(`server connected to ${port}`);
+    
+})
